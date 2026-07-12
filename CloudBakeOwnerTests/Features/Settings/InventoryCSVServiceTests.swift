c3fb4443c0ac7becfc5af6e4e70ce8fe44d39504
ygpramod <ygpramod@gmail.com>
2026-07-12 21:39:43 +0800

    Keep logo persistence tests focused

--- CloudBakeOwnerTests/Features/Settings/InventoryCSVServiceTests.swift
+++ CloudBakeOwnerTests/Features/Settings/InventoryCSVServiceTests.swift
@@ -1,28 +1,7 @@
 import XCTest
-import UIKit
 @testable import CloudBakeOwner
 
 final class InventoryCSVServiceTests: XCTestCase {
-    func testAppLogoStoreSavesLoadsAndRemovesSelectedImage() throws {
-        let directory = FileManager.default.temporaryDirectory
-            .appendingPathComponent(UUID().uuidString, isDirectory: true)
-        let fileURL = directory.appendingPathComponent("logo.jpg")
-        defer { try? FileManager.default.removeItem(at: directory) }
-        let store = AppLogoStore(fileURL: fileURL)
-        let image = UIGraphicsImageRenderer(size: CGSize(width: 20, height: 20)).image { context in
-            UIColor.systemPink.setFill()
-            context.fill(CGRect(x: 0, y: 0, width: 20, height: 20))
-        }
-
-        try store.save(image)
-
-        XCTAssertNotNil(store.load())
-
-        try store.remove()
-
-        XCTAssertNil(store.load())
-    }
-
     func testRecipeExportIncludesIgnoredExampleAndPipeSeparatedIngredients() throws {
         let repository = FakeRecipeRepository()
         let now = Date(timeIntervalSince1970: 1_800_000_000)
